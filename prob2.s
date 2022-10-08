288c920f8ce3fa2daaf1f18891d46066188f2b45
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2022-10-08 16:18:56 -0300

    cnpj  finalizado e comentariosiniciados

--- prob2.s
+++ prob2.s
@@ -1,77 +1,75 @@
 .data
-vetor: .word 8 6 8 6 4 8 4 0 0 0 0 1 1 8 # 86.864.840/0001-18
+vetor: .word 7 0 0 7 6 1 6 3 6 4 4
 .text
 main:
 la x12, vetor
-addi x13, x0, 14
-jal x1, verificacnpj
+addi x13, x0, 11
+jal x1, verificacpf
 beq x0, x0, FIM
 ##### START MODIFIQUE AQUI START #####
 
-cadastro_invalido:
-add x10, x0, x0
-jalr x0, 0(x1)
+checa_resto: # Checa o resto da divisão dos valores obtidos conforme os algoritmos
+blt x6, x19, menor_que_2 # Caso o resto, armazenado em x6, seja menor que 2, armazenado no registrador x19
+bge x6, x19, maior_ou_igual_a_2 # Caso o resto, armazenado em x6, seja maior ou igual que 2, armazenado no registrador x19
 
-cadastro_valido:
-addi x10, x0, 1
-jalr x0, 0(x1)
+menor_que_2: # Atribui os valores conforme o caso em que o resto seja menor que 2
+add x8, x0, x0 # O valor na posição referente à chamada no registro deve ser 0
+jalr x0, 0(x1) # Retorna a x1
 
-soma_digitos_cpf:
-lw x24, 0(x13)
-mul x3, x24, x22
-add x5, x3, x5
-addi x13, x13, -4
-addi x22, x22, 1
-bge x13, x12, soma_digitos_cpf
-jalr x0, 0(x1)
+maior_ou_igual_a_2: # Atribui os valores conforme o caso em que o resto seja maior ou igual a 2
+neg x9, x6 # x9 recebe x6 negativado
+addi x8, x9, 11 # O valor na posição referente à chamada no registro deve ser x9 + 11, vulgo 11 - x6 (resto da divisão) 
+jalr x0, 0(x1) # Retorna a x1
 
-checa_resto:
-blt x6, x19, menor_que_2
-bge x6, x19, maior_ou_igual_a_2
+cadastro_invalido: # Instrução que, caso o cadastro seja inválido, irá redirecionar para o final da execução com retorno 0
+add x10, x0, x0 # Atribui o valor 0 ao registrador de retorno x10
+jalr x0, 0(x1) # Retorna a x1
 
-menor_que_2:
-add x8, x0, x0 # penultimo valor igual a 0
-jalr x0, 0(x1)
+cadastro_valido: # Instrução que, caso o cadastro seja inválido, irá redirecionar para o final da execução com retorno 1
+addi x10, x0, 1 # Atribui o valor 1 ao registrador de retorno x10
+jalr x0, 0(x1) # Retorna a x1
 
-maior_ou_igual_a_2:
-neg x9, x6 # negativa x6
-addi x8, x9, 11 # x8 recebe 11 - x6, penultimo valor 
-jalr x0, 0(x1)
+soma_digitos_cpf: # Instrução que soma os dígitos do CPF conforme o algoritmo conhecido
+lw x24, 0(x13) # Carrega em x24 o valor de x13, que representa a posição do vetor do cadastro que estams no momento
+mul x3, x24, x22 # Multiplica o valor naquela posição do vetor pelo valor atual conforme o algoritmo, armazenado em x22
+add x5, x3, x5 # Adiciona o valor obtido no contador disponibilizado no registrador x5
+addi x13, x13, -4 # Decrementa uma posição no ponteiro do vetor
+addi x22, x22, 1 # Adiciona 1 à constante utilizada para os cálculos do algoritmo
+bge x13, x12, soma_digitos_cpf # Se o ponteiro de varredura vetor for maior ou igual à posição inicial do mesmo, chama o procedimento novamente
+jalr x0, 0(x1) # Retorna a x1
 
-verificacpf:
-addi x13, x13, -1
+verificacpf: # Instrução que verifica o CPF
+addi x11, x0, 11 # x11 recebe o valor da constante 11, útil para contas nos algoritmos de verificação de cadastro
+addi x19, x0, 2 # x19 recebe o valor da constante 2, útil para contas nos algoritmos de verificação de cadastro
+addi x13, x13, -1 # Essa e as próximas 2 linhas configuram x13 para ser a posição final do vetor, conforme seu tamanho
 slli x13, x13, 2
 add x13, x12, x13
-addi sp, sp, -8
-sw x13, 0(sp)
-addi x13, x13, -8
-add x5, x0, x0 # x5  = 0
-addi x22, x0, 2 # x22 = 2
-addi sp, sp, -8
-sw x1, 0(sp)
-jal x1, soma_digitos_cpf
-addi x11, x0, 11 # X11 tem valor 11
-addi x19, x0, 2 # x12 tem valor 2
-remu x6, x5, x11 # x6 é x5mod11
-jal x1, checa_resto
-lw x20, 8(sp)
-addi x20, x20, -4
-lw x21, 0(x20)
-bne x21, x8, cadastro_invalido
-add x13, x0, x20
-add x5, x0, x0 # x5  = 0
-addi x22, x0, 2 # x22 = 2
-jal x1, soma_digitos_cpf
-addi x11, x0, 11 # X11 tem valor 11
-addi x19, x0, 2 # x12 tem valor 2
-remu x6, x5, x11 # x6 é x5mod11
-jal x1, checa_resto
-lw x1, 0(sp)
-lw x20, 8(sp)
-addi sp, sp, 16
-lw x21, 0(x20)
-bne x21, x8, cadastro_invalido
-beq x0, x0, cadastro_valido
+addi sp, sp, -16 # Aloco dois espaços na pilha
+sw x13, 8(sp) # Armazeno o valor de endereço do final do vetor na segunda posição da pilha
+sw x1, 0(sp) # Armazeno o valor de x1 (caminho de instrução para finalizar a execução) no topo da pilha
+addi x13, x13, -8 # x13 agora armazena o endereço da antepenúltima posição do vetor (importante para a verificação dos cadastros)
+add x5, x0, x0 # x5 será um contador e será inicializado com 0
+addi x22, x0, 2 # x22 será uma variável auxiliar para as contas de verificação e é inicializada com 2
+jal x1, soma_digitos_cpf # Pulo para a instrução de soma de dígitos de CPF e linko x1 à próxima instrução
+remu x6, x5, x11 # x6 recebe o resto deixado pela soma dos dígitos do CPF (armazenado em x5) na divisão por 11
+jal x1, checa_resto # Pulo para a instrução de checagem de resto e linko x1 à próxima instrução
+lw x1, 0(sp) # Armazeno em x1 o valor do topo da pilha, caminho de instrução para finalizar a execução armazenado anteriormente na memória
+lw x20, 8(sp) # Armazeno em x20 o segundo valor na pilha, endereço do final do vetor de cadastro armazenado anteriormente
+addi x20, x20, -4 # x20 recebe agora o endereço da penúltima posição do vetor de cadastro
+lw x21, 0(x20) # x21 recebe o valor na penúltima posição do vetor de cadastro
+bne x21, x8, cadastro_invalido # Se o valor calculado para o penúltimo valor do cadastro for diferente do valor passado, o cadastro é inválido
+add x13, x0, x20 # x13 recebe agora o endereço da penúltima posição do vetor de cadastro
+add x5, x0, x0 # x5 será um contador e será inicializado com 0
+addi x22, x0, 2 # x22 será uma variável auxiliar para as contas de verificação e é inicializada com 2
+jal x1, soma_digitos_cpf # Pulo para a instrução de soma de dígitos de CPF e linko x1 à próxima instrução
+remu x6, x5, x11 # x6 recebe o resto deixado pela soma dos dígitos do CPF (armazenado em x5) na divisão por 11
+jal x1, checa_resto # Pulo para a instrução de checagem de resto e linko x1 à próxima instrução
+lw x1, 0(sp) # Armazeno em x1 o valor do topo da pilha, caminho de instrução para finalizar a execução armazenado anteriormente na memória
+lw x20, 8(sp) # Armazeno em x20 o segundo valor na pilha, endereço do final do vetor de cadastro armazenado anteriormente
+addi sp, sp, 16 # Desaloco duas posições na pilha
+lw x21, 0(x20) # x21 recebe o valor na última posição do vetor de cadastro
+bne x21, x8, cadastro_invalido # Se o valor calculado para o último valor do cadastro for diferente do valor passado, o cadastro é inválido
+beq x0, x0, cadastro_valido # Se chegamos nessa intrução, nosso cadastro é válido e chamaos a instrução feita para esse caso
 
 soma_digitos_cnpj:
 lw x24, 0(x13)
@@ -96,7 +94,7 @@ addi sp, sp, -8
 sw x13, 0(sp)
 addi x13, x13, -8
 add x5, x0, x0 # x5  = 0
-addi x22, x0, 0 # x22 = 0
+addi x22, x0, 2 # x22 = 2
 addi sp, sp, -8
 sw x1, 0(sp)
 jal x1, soma_digitos_cnpj
@@ -104,6 +102,7 @@ addi x11, x0, 11 # x11 tem valor 11
 addi x19, x0, 2 # x12 tem valor 2
 remu x6, x5, x11 # x6 é x5mod11
 jal x1, checa_resto
+lw x1, 0(sp)
 lw x20, 8(sp)
 addi x20, x20, -4
 lw x21, 0(x20)
