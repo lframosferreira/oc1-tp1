90c3b3bd2d6a8662efca4395a9da1406c829e5d3
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2022-10-16 10:52:51 -0300

    Finalização dos comentários de código no prob1

--- prob1.s
+++ prob1.s
@@ -18,7 +18,7 @@ lw x11, 0(x13) # x10 recebe o valor na posição final do vetor
 sw x9, 0(x13) # a posição final do vetor recebe o valor de x9, que guarda o valor da posição inicial do vetor
 sw x11, 0(x12) # a posição inicial do vetor receb o valor de x10, que guarda o valor da posição final do vetor
 addi x13, x13, -4 # "encurtamos" o vetor, considerando que ele termina na posição anterior à posição final prévia
-addi x12, x12, 4 # "encurtamos" o vetor, considerando que ele começa na posição subsequente à posição finicial prévia
+addi x12, x12, 4 # "encurtamos" o vetor, considerando que ele começa na posição subsequente à posição inicial prévia
 bge x13, x12, inverte # se a posição final for maior ou igual que a inicial, continuamos o procedimento
 jalr x0, 0(x1) # retornamos à instrução na posição guardada em x1
 ##### END MODIFIQUE AQUI END #####
